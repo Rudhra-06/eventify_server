9c3f1797898a79bd962868e1ca2c03665c31982b
Rudhrashini <rudhrashini@example.com>
2025-12-19 12:32:40 +0530

    4.0

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.log(err));
-
